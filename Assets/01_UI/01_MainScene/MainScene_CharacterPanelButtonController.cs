f5c39d9a9b1bcaa2b74b9c3437d09bcbecda9634
Pray <wnsgur567@naver.com>
2021-08-15 22:57:33 +0900

    main scene 수정중

---
+++ Assets/01_UI/01_MainScene/MainScene_CharacterPanelButtonController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScene_CharacterPanelButtonController : MonoBehaviour
+{
+    [SerializeField] Image m_main_button_panel;
+
+    // 선택 버튼 관련된 것은 character select manager 를 참조
+
+    public void __OnBackButton()
+    {
+        m_main_button_panel.gameObject.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+
+    public void __OnSelectCompleteButton()
+    {
+        // 다음 씬으로 이동 할 것 (로딩 씬 부르기)
+        List<string> loadScenes = new List<string>();
+        List<string> unloadScenes = new List<string>();
+    }
+}
---
+++ Assets/01_UI/01_MainScene/MainScene_MainPanelButtonController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScene_MainPanelButtonController : MonoBehaviour
+{
+    [SerializeField] Image m_character_panel;    
+    [Space(10)]
+    [SerializeField] Image m_option_panel;    
+
+    public void __OnStartButton()
+    {
+        m_character_panel.gameObject.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+    public void __OnExitButton()
+    {
+        // TODO : Save
+        Application.Quit();
+    }
+}
