5bcc2fe47ed13be388bfe7ae850d1d4ea03257d8
Pray <wnsgur567@naver.com>
2021-08-17 12:19:45 +0900

    stage animation update (TODO : staget table's icon field update / now , table hasn't icon field)

--- Assets/01_UI/13_Combination/Stage/StageIconSlot.cs
+++ Assets/01_UI/13_Combination/Stage/StageIconSlot.cs
@@ -5,14 +5,51 @@ using UnityEngine.UI;
 
 public class StageIconSlot : MonoBehaviour
 {
-    private Image m_image;
+    [SerializeField] Sprite_TableExcelLoader m_sprite_loader;
+    [SerializeField] Image m_image;
+    private RectTransform m_rt;
+
     private void Awake()
     {
-        m_image = this.GetComponent<Image>();
+        m_rt = this.GetComponent<RectTransform>();
+        m_rt.anchorMin = new Vector2(0f, 1f);
+        m_rt.anchorMax = new Vector2(0f, 1f);
     }
 
-    public void ChnageImage(Sprite sprite)
+    public void SetSize(Vector2 size)
     {
+        m_rt.sizeDelta = size;
+    }
+
+    public void SetPosition(float anckor_x)
+    {
+        m_rt.anchoredPosition = new Vector2(anckor_x, 0);
+    }
+    public float GetAnckorX()
+    {
+        return m_rt.anchoredPosition.x;
+    }
+
+    public void ChangeImage(int sprite_code)
+    {
+        Sprite sprite = m_sprite_loader.GetSprite(sprite_code);
         m_image.sprite = sprite;
+    }
+
+    // 0 -1f
+    public void SetImageAlpha(float alpha)
+    {
+        Color color = m_image.color;
+        color.a = alpha;
+        m_image.color = color;
+    }
+
+    public Color GetImageColor()
+    {
+        return m_image.color;
+    }
+    public void SetImageColor(Color color)
+    {
+        m_image.color = color;
     }
 }
