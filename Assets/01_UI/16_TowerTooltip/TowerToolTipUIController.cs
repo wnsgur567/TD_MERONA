323243fc2be796eda11a7c26ffbe759fd7517e71
Pray <wnsgur567@naver.com>
2021-08-18 09:31:34 +0900

    tower tooltip off update

--- Assets/01_UI/16_TowerTooltip/TowerToolTipUIController.cs
+++ Assets/01_UI/16_TowerTooltip/TowerToolTipUIController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class TowerToolTipUIController : MonoBehaviour
@@ -158,8 +159,33 @@ public class TowerToolTipUIController : MonoBehaviour
         }        
     }
 
+
+    [Space(30)]
+    [SerializeField] GraphicRaycaster m_Raycaster;
+    [SerializeField] EventSystem m_EventSystem;
+    PointerEventData m_PointerEventData;
+
     private void Update()
     {
-        
+        // check click position is out of this panel
+        if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            m_PointerEventData = new PointerEventData(m_EventSystem);
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            m_Raycaster.Raycast(m_PointerEventData, results);
+
+            foreach (RaycastResult result in results)
+            {
+                if(result.gameObject.tag == "TowerToolTip")
+                {
+                    return;
+                }
+            }
+
+            // out of panel
+            TowerToolTipManager.Instance.DeActivateTooltip();
+        }
     }
+
 }
